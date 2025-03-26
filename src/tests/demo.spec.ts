f011e8bb2100e0343c141d5fcaa68627e62fffec
David Numa <david.numa@gmail.com>
2025-03-25 22:12:34 -0400

    added pending files

---
+++ src/tests/demo.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from '@fixtures/testExtended.fixture';
+import { LoginPage } from '@pages/login.page';
+import { User } from '@interfaces/user.interface';
+
+test.beforeEach(async ({ page, printRandomQuote }) => {
+  await page.goto('/');
+  await Promise.all([
+    page.waitForLoadState('domcontentloaded'), // Wait for DOM to be ready
+    page.waitForLoadState('networkidle')      // Wait for all network activity to stop
+  ]);
+});
+
+test.afterEach(async ({ page }) => {
+  const timestamp = Date.now();
+  const path = `./src/screenshots/screenshot-${timestamp}.png`;
+  await page.screenshot({ path: path });
+});
+
+test.describe('Test Suite', {
+  tag: ['@suite', '@smoke'],
+  annotation: [ 
+    { type: 'issue', description: 'JIRA-1234' },
+  ]
+}, () => {
+
+  test('has title', async ({ page }) => {  
+    await expect(page).toHaveTitle('Swag Labs');
+  });
+
+  test('login', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const user: User = {
+      username: process.env.PLAYWRIGHT_USERNAME || 'standard_user',
+      password: process.env.PLAYWRIGHT_PASSWORD || 'secret_sauce'
+    };
+
+    await loginPage.login(user);
+
+    await expect(page).toHaveTitle('Swag Labs');
+  });
+
+});
+
+
+
