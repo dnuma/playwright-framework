eb7dfaec45c0cf212193e0a208621f1a6ef6f63c
David Numa <david.numa@gmail.com>
2025-03-25 21:49:47 -0400

    env installed and fixed, multiple addons

--- src/fixtures/testExtended.fixture.ts
+++ src/fixtures/testExtended.fixture.ts
@@ -1,13 +1,28 @@
 import { test as base, Page } from '@playwright/test';
 import { APP_URLS } from '@constants/url.constants';
+import { ApiCalls } from '@utils/apiCalls.utils';
 
 type LoginFixture = {
   login: (page: Page) => Promise<void>;
 };
 
-export const test = base.extend<LoginFixture>({
-  login: async ({ page }, use) => {
-    await page.goto(APP_URLS.login);
+type PrintStarWarsQuoteFixture = {
+  printStarWarsQuote: (page: Page) => Promise<void>;
+};
+
+export const test = base.extend<LoginFixture & PrintStarWarsQuoteFixture>({
+  login: async ({ page, baseURL }, use) => {
+    // Implement login logic here
+    console.log(`Logging in to: ${baseURL}`, APP_URLS.login);    
     await use(async () => {});
   },
+
+  printStarWarsQuote: async ({ request }, use) => {
+    const apiCalls = new ApiCalls();
+    const response = await apiCalls.starWarsQuote(request);
+    console.log(response);
+    await use(async () => {});
+  }
 });
+
+export { expect } from '@playwright/test';
--- src/pages/login.page.ts
+++ src/pages/login.page.ts
@@ -1,12 +1,24 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 import { BasePage } from "@pages/base.page";
 
 export class LoginPage extends BasePage {
   readonly page: Page;
+  readonly username: Locator;
+  readonly password: Locator;
+  readonly loginButton: Locator;
 
   constructor (page: Page) { 
     super(page);
     this.page = page;
+    this.username = page.locator('[data-test="username"]');
+    this.password = page.locator('[data-test="password"]');
+    this.loginButton = page.locator('[data-test="login-button"]');
   }
+
+  async login(username: string, password: string) {
+    await this.username.fill(username);
+    await this.password.fill(password);
+    await this.loginButton.click();
+  };
 
 }
